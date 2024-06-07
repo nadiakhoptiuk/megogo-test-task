bcc260243c4ced873f90a5739dea3a5b6f016223
nadiakhoptiuk <98387901+nadiakhoptiuk@users.noreply.github.com>
2024-06-07 10:50:19 +0000

    Deploying to gh-pages from @ nadiakhoptiuk/megogo-test-task@7ee7274df05e58616beaf71dd0cdcb157f2b835c 🚀

---
+++ index.3db7c9dc.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"vertical",slidesPerView:1,mousewheel:!0});
+//# sourceMappingURL=index.3db7c9dc.js.map
---
+++ index.e0233eeb.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"vertical",slidesPerView:1,mousewheel:!0});
+//# sourceMappingURL=index.e0233eeb.js.map
